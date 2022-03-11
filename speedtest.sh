0feaaeacdf999c441658bc49d4446e1d18a6f7cc
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2022-03-11 17:06:46 +0700

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
+rm speedtest.sh
 curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
 sudo apt-get install speedtest 
 clear
 echo ""
+echo "  Cài Đặt Speedtest Thành Công "
 echo " ---------------------------------------------- "
 echo "  Bấm Y để đồng ý nếu là cài đặt lần đầu "
 echo " ---------------------------------------------- "
